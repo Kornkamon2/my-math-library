5af439892d81a768d948c133c8c33b4ece73dcf8
kornkamon.chai <kornkamon.chai@dome.tu.ac.th>
2023-11-11 11:20:31 +0700

    Add array for each

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-function sum(a, b){
+function sum(a, b) {
+    if (Array.isArray(a)) {
+    let sum = 0
+    a.forEach((i) => (sum += i))
+    return sum
+    } else if (typeof a === 'number' && typeof b === 'number') {
     return a + b
-}
-
-module.exports = {
-    sum,
-}
+    }
+   }
+   
